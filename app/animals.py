c0fd8fa4e05ef29282913240ac9c0172f0d5d147
Eliemer <eliemer619@gmail.com>
2019-05-11 20:57:24 -0400

    cleaned csv index

--- app/animals.py
+++ app/animals.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import random
 
-animals = pd.read_csv('big_animal.csv', index_col=0 , header=None, names=['Scientific Names'])
+animals = pd.read_csv('big_animals.csv', header=None, names=['Scientific Names'])
 
 # print(animals.head())
 def getFursona():
